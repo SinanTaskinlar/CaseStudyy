abfe319fa3a4aeec4a1ac0614104f81811bcf1d7
Sinan Taşkınlar <sinantasknlar@gmail.com>
2023-03-11 05:20:52 +0300

    Add files via upload

---
+++ KaizenCase/KaizenCase/Program.cs
@@ -0,0 +1,76 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Oluşturulacak hediye çeki kodu sayısını kullanıcıdan al
+        Console.Write("Kaç adet hediye çeki kodu oluşturmak istiyorsunuz? ");
+        int? numberOfGiftCodes = int.Parse(Console.ReadLine());
+
+        // Hediye çeki kodları oluşturuluyor ve ekrana yazdırılıyor
+        for (int i = 0; i < numberOfGiftCodes; i++)
+        {
+            string giftCode = GenerateGiftCode();
+            Console.WriteLine($"Oluşturulan hediye çeki kodu: {giftCode}");
+        }
+
+        // Doğrulanacak hediye çeki kodunu kullanıcıdan al
+        Console.Write("Doğrulamak istediğiniz hediye çeki kodunu girin: ");
+        string? giftCodeToValidate = Console.ReadLine();
+
+        // Hediye çeki kodu doğrulanıyor ve sonuç ekrana yazdırılıyor
+        var isValidGiftCode = ValidateGiftCode(giftCodeToValidate);
+        if (isValidGiftCode)
+        {
+            Console.WriteLine("Hediye çeki kodu geçerlidir.");
+        }
+        else
+        {
+            Console.WriteLine("Hediye çeki kodu geçersizdir.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static string GenerateGiftCode()
+    {
+        string chars = "ACDEFGHKLMNPRTXYZ234579";
+        var random = new Random();
+        string giftCode = new string(
+            Enumerable.Repeat(chars, 5)
+                      .Select(s => s[random.Next(s.Length)])
+                      .ToArray());
+
+        int checksum = 0;
+        for (int i = 0; i < giftCode.Length; i++)
+        {
+            checksum += (int)giftCode[i]; // ASCII değerleri toplamı hesaplanıyor
+        }
+
+        // Doğrulama kodu olarak son 3 hane, ASCII değerleri toplamının son 3 hanesi kullanılıyor
+        string checksumStr = checksum.ToString();
+        string validationCode = checksumStr.Substring(checksumStr.Length - 3);
+
+        return giftCode + validationCode; // Hediye çeki kodu, doğrulama kodu ile birlikte döndürülüyor
+    }
+
+    static bool ValidateGiftCode(string giftCode)
+    {
+        if (string.IsNullOrEmpty(giftCode) || giftCode.Length != 8)
+        {
+            return false;
+        }
+
+        string giftCodeWithoutValidation = giftCode.Substring(0, giftCode.Length - 3);
+        int checksum = 0;
+        for (int i = 0; i < giftCodeWithoutValidation.Length; i++)
+        {
+            checksum += (int)giftCodeWithoutValidation[i]; // ASCII değerleri toplamı hesaplanıyor
+        }
+
+        // Doğrulama kodu olarak son 3 hane, ASCII değerleri toplamının son 3 hanesi kullanılıyor
+        string checksumStr = checksum.ToString();
+        string validationCode = checksumStr.Substring(checksumStr.Length - 3);
+
+        return giftCode.Substring(giftCode.Length - 3) == validationCode; // Hediye çeki kodunun doğruluğu kontrol ediliyor
+    }
+}
---
+++ KaizenCase/KaizenCase2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+string json = System.IO.File.ReadAllText(@"response.json");
+
+
+List<Root> items = JsonConvert.DeserializeObject<Root[]>(json).ToList();
+char[] splitters = new char[] { '\n', ' ' };
+List<string> x = items[0].description.Split(splitters, StringSplitOptions.RemoveEmptyEntries).ToList();
+int LineNumber = 1;
+Console.Write(LineNumber.ToString() + " " + items[1].description + "\n" + (++LineNumber).ToString() + " ");
+
+for (int i = 2; i < items.Count - 1; i++)
+{
+    var asd = items[i].boundingPoly.vertices[0].y;
+    var asd2 = items[i - 1].boundingPoly.vertices[0].y;
+
+    if (asd - asd2 > 15) //alt köşelerdeki sapma değerini 15 olarak düşün.
+    {
+        LineNumber++;
+        Console.Write("\n");
+        Console.Write(LineNumber.ToString() + " " + items[i].description);
+    }
+    else
+    {
+        Console.Write(" " + items[i].description);
+    }
+}
+
+Console.ReadLine();
+
+public class Root
+{
+    public string? locale { get; set; }
+    public string? description { get; set; }
+    public BoundingPoly? boundingPoly { get; set; }
+}
+public class BoundingPoly
+{
+    public List<Vertex>? vertices { get; set; }
+}
+public class Vertex
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
